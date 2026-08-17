95ba830afe0218ad6453389557e8028b0f5fe3b9
jimy-prog <82962216+jimy-prog@users.noreply.github.com>
2026-08-18 00:11:13 +0500

    Add hooks, lib, store index, i18n locales

---
+++ src/i18n/index.ts
@@ -0,0 +1,27 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import ru from './locales/ru.json';
+import en from './locales/en.json';
+import uz from './locales/uz.json';
+
+const resources = {
+  ru: { translation: ru },
+  en: { translation: en },
+  uz: { translation: uz },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'ru',
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
